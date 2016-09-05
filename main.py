5ff97d239f591ac7ce884eeb5ba5233a63d7e7f5
TheTycoon <jsbalebo@gmail.com>
2016-09-05 10:01:14 -0600

    tweaks

--- main.py
+++ main.py
@@ -119,7 +119,11 @@ class Game:
             pygame.draw.line(self.screen, WHITE, (0, y), (WIDTH, y))
 
     # Double Check These Functions / Make Better - Use x & y to draw text position as well
-    def draw_health_bar(self, self_screen, x, y, percentage):
+    def draw_bar(self, self_screen, x, y, percentage, type):
+        if type == 'health':
+            color = RED
+        if type == 'mana':
+            color = BLUE
         if percentage < 0:
             percentage = 0
         BAR_LENGTH = 100
@@ -127,21 +131,13 @@ class Game:
         filled = percentage * BAR_LENGTH
         outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
         filled_rect = pygame.Rect(x, y, filled, BAR_HEIGHT)
-        pygame.draw.rect(self_screen, RED, filled_rect)
+        pygame.draw.rect(self_screen, color, filled_rect)
         pygame.draw.rect(self_screen, WHITE, outline_rect, 2)
-        self.draw_text(self.screen, "Health", 12, WHITE, WIDTH - 130, HEIGHT - 36, True)
 
-    def draw_mana_bar(self, self_screen, x, y, percentage):
-        if percentage < 0:
-            percentage = 0
-        BAR_LENGTH = 100
-        BAR_HEIGHT = 10
-        filled = percentage * BAR_LENGTH
-        outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
-        filled_rect = pygame.Rect(x, y, filled, BAR_HEIGHT)
-        pygame.draw.rect(self_screen, BLUE, filled_rect)
-        pygame.draw.rect(self_screen, WHITE, outline_rect, 2)
-        self.draw_text(self.screen, "Mana", 12, WHITE, WIDTH - 130, HEIGHT - 21, True)
+        if type == 'health':
+            self.draw_text(self.screen, "Health", 12, WHITE, WIDTH - 130, HEIGHT - 36, True)
+        if type == 'mana':
+            self.draw_text(self.screen, "Mana", 12, WHITE, WIDTH - 130, HEIGHT - 21, True)
 
     def draw_enemy_info(self):
         image = self.player.selected_mob.image
@@ -150,13 +146,16 @@ class Game:
         image_rect.y = HEIGHT - 36
         self.screen.blit(image, image_rect)
         self.draw_text(self.screen, (self.player.selected_mob.name + ": Level " + str(self.player.selected_mob.level)), 12, WHITE, WIDTH / 2 + 50, HEIGHT - 36, False)
-        self.draw_health_bar(self.screen, WIDTH / 2 + 50, HEIGHT - 20, self.player.selected_mob.current_hit_points / self.player.selected_mob.max_hit_points)
+        self.draw_bar(self.screen, WIDTH / 2 + 50, HEIGHT - 20,
+                      self.player.selected_mob.current_hit_points / self.player.selected_mob.max_hit_points, 'health')
 
     def draw_ui(self):
+        # Draw Empty Action Bar
         pygame.draw.rect(self.screen, GRAY, (0, HEIGHT - 40, WIDTH, 40))
         for i in range(10):
             pygame.draw.rect(self.screen, SILVER, (5 + ((5 + TILESIZE) * i), HEIGHT - 36, 32, 32))
 
+        # Draw Icons in Action Bar
         attack_icon = self.attack_icon
         attack_rect = attack_icon.get_rect()
         attack_rect.x = 5
@@ -169,8 +168,20 @@ class Game:
         magic_rect.y = HEIGHT - 36
         self.screen.blit(magic_icon, magic_rect)
 
+        # Draw Numbers in Action Bar
         for i in range(10):
             self.draw_text(self.screen, str((i + 1) % 10), 12, BLACK, 10 + ((5 + TILESIZE) * i), HEIGHT - 36, True)
+
+        # Draw Health and Mana Bars
+        self.draw_bar(self.screen, WIDTH - 105, HEIGHT - 35,
+                      self.player.current_hit_points / self.player.max_hit_points, 'health')
+        self.draw_bar(self.screen, WIDTH - 105, HEIGHT - 20,
+                      self.player.current_mana_points / self.player.max_mana_points, 'mana')
+
+        # Draw a box to highlight the selected enemy and show enemy info in action bar
+        if self.machine.state == 'player_turn_attack' or self.machine.state == 'player_turn_magic':
+            pygame.draw.rect(self.screen, ORANGE, self.player.selected_mob.rect, 3)
+            self.draw_enemy_info()
 
     # function to handle drawing all types text
     def draw_text(self, surface, text, size, color, x, y, centered):
@@ -198,18 +209,12 @@ class Game:
         if self.machine.state == 'player_turn_magic':
             self.player.draw_range_area(self.player.spell_range, 'filled', LIGHT_BLUE)
 
+        # Draw all sprites, the grid, and the UI
         self.all_sprites.draw(self.screen)
         self.draw_grid()
         self.draw_ui()
-        self.draw_health_bar(self.screen, WIDTH - 105, HEIGHT - 35,
-                             self.player.current_hit_points / self.player.max_hit_points)
-        self.draw_mana_bar(self.screen, WIDTH - 105, HEIGHT - 20,
-                           self.player.current_mana_points / self.player.max_mana_points)
 
-        # Draw a box to highlight the selected enemy and show enemy info in action bar
-        if self.machine.state == 'player_turn_attack' or self.machine.state == 'player_turn_magic':
-            pygame.draw.rect(self.screen, ORANGE, self.player.selected_mob.rect, 3)
-            self.draw_enemy_info()
+
 
         # DISPLAY FRAME = STOP DRAWING
         pygame.display.flip()
--- spells.py
+++
@@ -1,6 +0,0 @@
-class Spell:
-    def __init__(self, name, power, type, cost):
-        self.name = name
-        self.range = power
-        self.range_type = type
-        self.cost = cost
